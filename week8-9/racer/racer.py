6e08054cee5166dc7d880ab512bf6215b6282955
Olzhas <o_sembinov@kbtu.kz>
2024-04-09 21:21:15 +0500

    test14

--- week8-9/racer/racer.py
+++ week8-9/racer/racer.py
@@ -1,80 +1,149 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
-import random
-import sys
-
-from pygame.sprite import _Group
-
+import random, time
+ 
+#Initialzing 
 pygame.init()
-
+ 
+#Setting up FPS 
 FPS = 60
 FramePerSec = pygame.time.Clock()
-
+ 
+#Creating colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
+ 
+#Other Variables for use in the program
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
-
-DISPLAYSURF = pygame.display.set_mode((400, 600))
+SPEED = 5
+SCORE = 0
+COINS = 0
+ 
+#Setting up Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+ 
+background = pygame.image.load("week8-9/racer/AnimatedStreet.png")
+ 
+#Create a white screen 
+DISPLAYSURF = pygame.display.set_mode((400,600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
-class Enemy(pygame.sprite.Sprite):
+class Coins(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Enemy.png")
+        self.image = pygame.image.load("week8-9/racer/coin.png")
         self.rect = self.image.get_rect()
-        self.rect.center=(random.randit(40,SCREEN_WIDTH-40), 0)
-    
-    def move(self):
-        self.rect.move_ip(0, 10)
-        if (self.rect.bottom > 600):
-            self.rect.top = 0
-            self.rect.center = (random.randint(30, 370), 0)
-    
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)
 
+    def move(self):
+        self.rect.move_ip(0,SPEED)
+        if (self.rect.top > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+ 
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("week8-9/racer/Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)  
+ 
+      def move(self):
+        global SCORE
+        self.rect.move_ip(0,SPEED)
+        if (self.rect.top > 600):
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+ 
+ 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("Player.png")
+        super().__init__() 
+        self.image = pygame.image.load("week8-9/racer/Player.png")
         self.rect = self.image.get_rect()
         self.rect.center = (160, 520)
-    
-    def update(self):
+        
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
-        #if pressed_keys[K_UP]:
+       #if pressed_keys[K_UP]:
             #self.rect.move_ip(0, -5)
-        #if pressed_keys[K_DOWN]:
+       #if pressed_keys[K_DOWN]:
             #self.rect.move_ip(0,5)
+         
         if self.rect.left > 0:
-            if pressed_keys[K_LEFT]:
-                self.rect.move_ip(-5, 0)
-        if self.rect.right < SCREEN_WIDTH:
-            if pressed_keys[K_RIGHT]:
-                self.rect.move_ip(5, 0)
-    
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(5, 0)
+                   
+#Setting up Sprites        
 P1 = Player()
 E1 = Enemy()
-
+C1 = Coins()
+ 
+#Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+coins = pygame.sprite.Group()
+coins.add(C1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+all_sprites.add(C1)
+ 
+#Adding a new User event 
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+ 
+#Game Loop
 while True:
+       
+    #Cycles through all events occurring  
     for event in pygame.event.get():
-        pygame.quit()
-        sys.exit()
-    P1.update()
-    E1.move()
+        if event.type == INC_SPEED:
+              SPEED += 0.5     
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+ 
+    DISPLAYSURF.blit(background, (0,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    coins = font_small.render(str(COINS), True, BLACK)
+    DISPLAYSURF.blit(scores, (10,10))
+    DISPLAYSURF.blit(coins, (30, 10))
+ 
+    #Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+ 
+    #To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+          pygame.mixer.Sound('week8-9/racer/crash.wav').play()
+          time.sleep(0.5)
+                    
+          DISPLAYSURF.fill(RED)
+          DISPLAYSURF.blit(game_over, (30,250))
+           
+          pygame.display.update()
+          for entity in all_sprites:
+                entity.kill() 
+          time.sleep(2)
+          pygame.quit()
+          sys.exit()       
 
-    DISPLAYSURF.fill(WHITE)
-    P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+    if pygame.sprite.spritecollideany(P1, coins):
+        COINS += 1
 
+         
     pygame.display.update()
     FramePerSec.tick(FPS)
